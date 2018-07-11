831c181352aa050e32e3f312478a2ca7f08810cc
Taylor Laine <taylor.laine@gyro.com>
2018-07-10 19:36:36 -0600

    ignore

--- Config/config.js
+++ Config/config.js
@@ -1,4 +1,4 @@
-const Nav = {
+const Config = {
     apiKey: "AIzaSyAsR8ryHhRcDeKqA3S2VAKl4Qx-XsSWFBk",
     authDomain: "taptap-5e974.firebaseapp.com",
     databaseURL: "https://taptap-5e974.firebaseio.com",
@@ -8,4 +8,4 @@ const Nav = {
 }
 
 
-export default Nav
+export default Config
